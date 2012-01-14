9cbcdbb2518e1e15e24622a2fe0041b31d99760c
Bartek Wilczynski <bwilczyn@gmail.com>
2012-01-14 16:36:45 +0100

    [#3] XPSliderDataCell sample, closes #3

--- ios-dataview/XPDataTable1ViewController.m
+++ ios-dataview/XPDataTable1ViewController.m
@@ -10,6 +10,7 @@
 #import "XPDataGroup.h"
 #import "XPDataCell.h"
 #import "XPSwitchDataCell.h"
+#import "XPSliderDataCell.h"
 
 @implementation XPDataTable1ViewController
 
@@ -31,9 +32,10 @@
     
     XPDataCell *cell1 = [[XPDataCell alloc] initWithText:@"Sample cell 1"];
     XPDataCell *cell2 = [[XPDataCell alloc] initWithText:@"Sample cell 2"];
-    XPSwitchDataCell *cell3 = [[XPSwitchDataCell alloc] initWithText:@"Sample cell 2" on:YES];
+    XPSwitchDataCell *cell3 = [[XPSwitchDataCell alloc] initWithText:@"Sample switch" on:YES];
+    XPSliderDataCell *cell4 = [[XPSliderDataCell alloc] initWithText:@"Sample slider" value:.5f];
     
-    group1.cells = [NSArray arrayWithObjects:cell1, cell2, cell3, nil];
+    group1.cells = [NSArray arrayWithObjects:cell1, cell2, cell3, cell4, nil];
     
     self.dataGroups = [NSArray arrayWithObjects:group1, nil];
     
